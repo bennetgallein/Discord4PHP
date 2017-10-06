f4bc9c971618f09ecbce9e6332d8a597a1da54d7
bennetgallein <info@team-scorpia.de>
2017-10-06 11:46:01 +0200

    Removed wrong stuff out of ChannelArray

--- src/Objects/Channel/ChannelArray.php
+++ src/Objects/Channel/ChannelArray.php
@@ -5,26 +5,5 @@ namespace Discord\Objects\Channel;
 */
 class ChannelArray {
 
-    public $id;
-    /*  GUILD_TEXT	0
-        DM	1
-        GUILD_VOICE	2
-        GROUP_DM	3
-        GUILD_CATEGORY	4
-    */
-    public $type;
-    public $guildid;
-    public $position;
-    public $permission_overwrites;
-    public $name;
-    public $topic;
-    public $nsfw;
-    public $last_message_id;
-    public $bitrate;
-    public $user_limit;
-    public $recipients;
-    public $icon;
-    public $owner_id;
-    public $application_id;
-    public $parent_id;
+    
 }
